4529fd4cbeecc4615a271328eac2787b0b8752ba
Naomi Guyer <naomi@silverstripe.com>
2012-11-09 12:16:34 +1300

    MINOR: Tidy up and comment code. Remove unused menu js
    Conflicts:
    	admin/css/screen.css

--- javascript/TabSet.js
+++ javascript/TabSet.js
@@ -17,23 +17,23 @@
 				this.rewriteHashlinks();
 				this.tabs();
 				
+				//Apply special behaviour to action tabs: closed by default, and collapsible
 				if(this.hasClass('ss-ui-action-tabset')){
 					this.tabs(
 						'option', 
 						'collapsible',
 						true					
-					).tabs('select', false).tabs(
-						"option", 
-						"show", 
-						{ effect: "slideDown", duration: 5000 }	
-					);					
-					if(this.hasClass('cms-actions-row')){
-						var parent = this.parent().parent();
-						$('#view-mode-batchactions').click(function(){
-							$(this).parent('li').toggleClass('checked');
-						});
+					).tabs('select', false);	
 
-						if($(parent).hasClass('cms-tree-view-sidebar')){							
+					//Apply special behaviour to the cms actions row				
+					if(this.hasClass('cms-actions-row')){
+						
+						/* If actions panel is within the tree, apply active class 
+						to help animate open/close on hover
+						Position must be reset else anyone coming from main sitetree 
+						will see broken tabs */
+						var container = this.parent().parent();
+						if($(container).hasClass('cms-tree-view-sidebar')){							
 							$('.ui-tabs-nav li').hover(function(){								
 								$(this).parent().find('li .active').removeClass('active');
 								$(this).find('a').addClass('active');															
@@ -45,10 +45,9 @@
 									$(activePanel).attr("style","left : auto; right: auto");																	
 								}					
 							});	
-
-
-
-						}else{							
+						}else{		
+							/* If the tabs are in the full site tree view, do some 
+							positioning so tabPanel stays with relevent tab */					
 							this.tabs({
 								beforeActivate:function(event, ui){
 									var activePanel = ui.newPanel;
@@ -56,10 +55,12 @@
 									if($(activeTab).hasClass("last")){
 										$(activePanel).attr("style","left : auto; right: "+ 0 +"px");
 									}else{
-										$(activePanel).attr("style","left: "+activeTab.position().left+"px");									
-									}								
-								}				
-							});	
+										if(activeTab.position()!=null){
+											$(activePanel).attr("style","left: "+activeTab.position().left+"px");									
+										}
+									}															
+								}
+							});
 						}
 					}
 				}
