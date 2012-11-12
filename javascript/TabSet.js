823e966696edb543bba5aa8e6c6e9a58bbbee0c4
Naomi Guyer <naomi@silverstripe.com>
2012-11-12 13:53:47 +1300

    ENHANCEMENT: Positioning and placing of south actiontabs

--- javascript/TabSet.js
+++ javascript/TabSet.js
@@ -4,6 +4,57 @@
 		 * Lightweight wrapper around jQuery UI tabs.
 		 */
 		$('.ss-tabset').entwine({
+
+			/*Custom functionality for special action tabsets*/
+			action_tabs: function(){
+				this.tabs(
+					'option', 
+					'collapsible',
+					true					
+				).tabs('select', false);
+				
+				//Check if tabs should open upwards, and adjust
+				this.rise_up();
+					
+				//Apply special behaviour to the cms actions row				
+				if(this.hasClass('cms-actions-row')){
+					
+					/* If actions panel is within the tree, apply active class 
+					to help animate open/close on hover
+					Position must be reset else anyone coming from main sitetree 
+					will see broken tabs */
+					var container = this.parent().parent();
+					if($(container).hasClass('cms-tree-view-sidebar')){							
+						$('.ui-tabs-nav li').hover(function(){								
+							$(this).parent().find('li .active').removeClass('active');
+							$(this).find('a').addClass('active');															
+						});
+
+						this.tabs({
+							beforeActivate:function(event, ui){
+								var activePanel = ui.newPanel;
+								$(activePanel).attr("style","left : auto; right: auto");																	
+							}					
+						});	
+					}else{		
+						/* If the tabs are in the full site tree view, do some 
+						positioning so tabPanel stays with relevent tab */					
+						this.tabs({
+							beforeActivate:function(event, ui){
+								var activePanel = ui.newPanel;
+								var activeTab = ui.newTab;
+								if($(activeTab).hasClass("last")){
+									$(activePanel).attr("style","left : auto; right: "+ 0 +"px");
+								}else{
+									if(activeTab.position()!=null){
+										$(activePanel).attr("style","left: "+activeTab.position().left+"px");									
+									}
+								}															
+							}
+						});
+					}
+				}
+			},
 			onadd: function() {
 				// Can't name redraw() as it clashes with other CMS entwine classes
 				this.redrawTabs();
@@ -13,56 +64,47 @@
 				if(this.data('uiTabs')) this.tabs('destroy');
 				this._super();
 			},
+			rise_up: function(){
+				/* Function checks to see if a tab should be opened upwards 
+				(based on space concerns. If true, the rise-up class is applied 
+				and the position is calculated and applied to the element */
+				var elHeight = $(this).find('.ui-tabs-panel').outerHeight();				
+				var trigger = $(this).find('.ui-tabs-nav').outerHeight();
+				var endOfWindow = ($(window).height() + $(document).scrollTop()) - trigger;
+				var elPos = $(this).find('.ui-tabs-nav').offset().top;
+				if(elPos + elHeight >= endOfWindow && elPos - elHeight > 0){
+					this.addClass('rise-up');	
+
+					/* Apply position to tab */
+					this.tabs({
+						beforeActivate:function(event, ui){
+							var activePanel = ui.newPanel;
+							var activeTab = ui.newTab;			
+							if(activeTab.position()!=null){
+								var top = -activePanel.outerHeight();
+								var containerSouth = activePanel.parents('.south');
+								if(containerSouth){
+									var padding = activeTab.offset().top-containerSouth.offset().top;								
+									top = top-padding;	
+								}						
+
+								$(activePanel).attr("style","top: "+top+"px");									
+							}
+						}
+					});				
+
+				}else{
+					this.removeClass('rise-up');
+				}
+				return false;		
+			},
 			redrawTabs: function() {
 				this.rewriteHashlinks();
 				this.tabs();
 				
 				//Apply special behaviour to action tabs: closed by default, and collapsible
-				if(this.hasClass('ss-ui-action-tabset')){
-					this.tabs(
-						'option', 
-						'collapsible',
-						true					
-					).tabs('select', false);	
-
-					//Apply special behaviour to the cms actions row				
-					if(this.hasClass('cms-actions-row')){
-						
-						/* If actions panel is within the tree, apply active class 
-						to help animate open/close on hover
-						Position must be reset else anyone coming from main sitetree 
-						will see broken tabs */
-						var container = this.parent().parent();
-						if($(container).hasClass('cms-tree-view-sidebar')){							
-							$('.ui-tabs-nav li').hover(function(){								
-								$(this).parent().find('li .active').removeClass('active');
-								$(this).find('a').addClass('active');															
-							});
-
-							this.tabs({
-								beforeActivate:function(event, ui){
-									var activePanel = ui.newPanel;
-									$(activePanel).attr("style","left : auto; right: auto");																	
-								}					
-							});	
-						}else{		
-							/* If the tabs are in the full site tree view, do some 
-							positioning so tabPanel stays with relevent tab */					
-							this.tabs({
-								beforeActivate:function(event, ui){
-									var activePanel = ui.newPanel;
-									var activeTab = ui.newTab;
-									if($(activeTab).hasClass("last")){
-										$(activePanel).attr("style","left : auto; right: "+ 0 +"px");
-									}else{
-										if(activeTab.position()!=null){
-											$(activePanel).attr("style","left: "+activeTab.position().left+"px");									
-										}
-									}															
-								}
-							});
-						}
-					}
+				if(this.hasClass('ss-ui-action-tabset')){				
+					this.action_tabs();
 				}
 			},
 		
